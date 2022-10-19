7dc75713666af61ed945ed62735eaf4dea2fc670
Juan Beccaceci <juanbeccaceci@gmail.com>
2022-10-19 12:18:52 -0300

    rest utils methods@

--- lib/utils/RestUtils.d.ts
+++ lib/utils/RestUtils.d.ts
@@ -1,5 +1,22 @@
 export declare class RestUtils {
+    /**
+     * Gets public file by url as arraybuffer
+     * @param document File to load
+     */
     getBase64File(document: string): Promise<any>;
+    /**
+     * Gets a public file by url and convert it into a base64 string
+     * @param document File to load
+     */
+    getBase64FileAsString(document: string): Promise<any>;
+    /**
+     * Get mimetype based on end of string
+     * @param file
+     */
     getFileMimetype(file: string): string;
+    /**
+     * Get mimetype by extension
+     * @param extension
+     */
     getMimetype(extension: string): string;
 }
--- src/utils/RestUtils.ts
+++ src/utils/RestUtils.ts
@@ -2,6 +2,10 @@ import axios from 'axios';
 
 export class RestUtils {
 
+    /**
+     * Gets public file by url as arraybuffer
+     * @param document File to load
+     */
     public async getBase64File(document: string): Promise<any> {
         if (!document) {
             throw new Error(`Document not defined`);
@@ -24,16 +28,49 @@ export class RestUtils {
         return pdfResponse;
     }
 
+    /**
+     * Gets a public file by url and convert it into a base64 string
+     * @param document File to load
+     */
+    public async getBase64FileAsString(document: string): Promise<any> {
+        if (!document) {
+            throw new Error(`Document not defined`);
+        }
+
+        const encoding = this.getFileMimetype(document);
+        let response: any;
+        try {
+            response = axios.get(document, {
+                responseType: 'arraybuffer',
+                responseEncoding: 'binary',
+                headers: {
+                    'Content-Type': encoding,
+                },
+            });
+        } catch (e) {
+            throw e;
+        }
+        console.log(`File obtained successfully ${document}`);
+        return Buffer.from(response.data).toString('base64');
+    }
+
+    /**
+     * Get mimetype based on end of string
+     * @param file
+     */
     public getFileMimetype(file: string): string {
         if (file.endsWith("pdf")) {
             return 'application/pdf';
         } else if (file.endsWith("png")) {
             return 'image/png';
         }
-
         return 'application/pdf';
     }
 
+    /**
+     * Get mimetype by extension
+     * @param extension
+     */
     public getMimetype(extension: string): string {
         switch (extension) {
             case 'pdf':
