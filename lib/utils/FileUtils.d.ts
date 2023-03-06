3149b20e16b9edd08292dab10ca05af2f4eb7975
Juan Beccaceci <juanbeccaceci@gmail.com>
2023-03-06 10:03:53 -0300

    request handler changed by api gw request handler

--- lib/utils/FileUtils.d.ts
+++ lib/utils/FileUtils.d.ts
@@ -1,12 +1,14 @@
-/**
- * Decode mimetype in Base64 string using "Magic Numbers"
- * @ref https://en.wikipedia.org/wiki/Magic_number_(programming)
- * @ref https://stackoverflow.com/questions/57976898/how-to-get-mime-type-from-base-64-string
- * @param base64 Base64 encoded string
- */
-export declare const get_base64_mimetype: (base64: string) => string;
-/**
- * Converts mimetype to file extension
- * @param mimetype Decoded mimetype
- */
-export declare const mimetype_to_extension: (mimetype: string) => string;
+export declare class FileUtils {
+    /**
+     * Decode mimetype in Base64 string using "Magic Numbers"
+     * @ref https://en.wikipedia.org/wiki/Magic_number_(programming)
+     * @ref https://stackoverflow.com/questions/57976898/how-to-get-mime-type-from-base-64-string
+     * @param base64 Base64 encoded string
+     */
+    static get_base64_mimetype: (base64: string) => string;
+    /**
+     * Converts mimetype to file extension
+     * @param mimetype Decoded mimetype
+     */
+    static mimetype_to_extension: (mimetype: string) => string;
+}
