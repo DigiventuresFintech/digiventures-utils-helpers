b81f3dccd0195872da824646d8f4def0d869e639
Juan Beccaceci <juanbeccaceci@gmail.com>
2023-06-21 20:40:02 -0300

    new endpoint added

--- src/internal/ApiWebhooksService.ts
+++ src/internal/ApiWebhooksService.ts
@@ -79,6 +79,31 @@ export class ApiWebhooksService {
         return response?.data;
     }
 
+    async getDocumentByMongoDBQuery(query: string, options: any = {}): Promise<any> {
+        if (!this.API_WEBHOOKS_BASE_URL) {
+            throw `api webhooks url not defined`;
+        }
+        options = Object.assign(options, {
+            'Content-Type': 'application/json',
+            workspace: options?.workspace || '62195d46c8b99af141555eb6',
+        });
+
+        const url = `${this.API_WEBHOOKS_BASE_URL}/1.0/legajos/query?${query}`;
+
+        let response: any;
+        try {
+            response = await axios.get(url, {
+                headers: {
+                    ...options,
+                },
+            });
+        } catch (error) {
+            console.log('getDocumentByMongoDBQuery error: ', error);
+            throw new Error('cannot get document');
+        }
+        return response?.data;
+    }
+
     async updateDocumentById(
         legajoId: string,
         data: any,
