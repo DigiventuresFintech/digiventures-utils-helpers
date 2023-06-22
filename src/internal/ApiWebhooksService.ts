a4cb49a4e692ae6c293f1fd02866c99ad3b4a69e
Juan Beccaceci <juanbeccaceci@icloud.com>
2023-06-21 22:23:42 -0300

    Mongodb query endpoint (#30)
    * new endpoint added
    
    * new endpoint added
    
    * fix env variable
    
    * fix env variable
    
    * fix
    
    ---------
    
    Co-authored-by: Juan Beccaceci <juanbeccaceci@gmail.com>

--- src/internal/ApiWebhooksService.ts
+++ src/internal/ApiWebhooksService.ts
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import { JSONUtils } from '../utils/JSONUtils';
 
 export class ApiWebhooksService {
     readonly API_WEBHOOKS_BASE_URL: string | undefined =
@@ -74,6 +73,31 @@ export class ApiWebhooksService {
             });
         } catch (error) {
             console.log('Document error: ', error);
+            throw new Error('cannot get document');
+        }
+        return response?.data;
+    }
+
+    async getDocumentByMongoDBQuery(query: string, options: any = {}): Promise<any> {
+        if (!this.API_WEBHOOKS_BASE_URL) {
+            throw `api webhooks url not defined`;
+        }
+        options = Object.assign(options, {
+            'Content-Type': 'application/json',
+            workspace: options?.workspace || '62195d46c8b99af141555eb6',
+        });
+
+        const url = `${this.API_WEBHOOKS_BASE_URL}/1.0/legajos/query?query=${query}`;
+
+        let response: any;
+        try {
+            response = await axios.get(url, {
+                headers: {
+                    ...options,
+                },
+            });
+        } catch (error) {
+            console.log('getDocumentByMongoDBQuery error: ', error);
             throw new Error('cannot get document');
         }
         return response?.data;
