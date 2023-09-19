ccfc83d0112ebab8e95fc2bca8769ece623ac177
Juan Beccaceci <juanbeccaceci@gmail.com>
2023-09-18 21:00:14 -0300

    fix ftp redeable stream

--- lib/client/ftp/FtpClientManager.d.ts
+++ lib/client/ftp/FtpClientManager.d.ts
@@ -5,9 +5,10 @@ import { Readable } from "stream";
 export declare class FtpClientManager implements IFtpClientManager {
     readonly client: Client;
     private options;
+    private workingDirectory;
     constructor(options: any);
     connect(): Promise<any>;
     put(origin: Readable | string, dest: string, createDir?: boolean): Promise<any>;
-    createSftpDirs(path: string): Promise<void>;
+    createSftpDirs(path: string): Promise<string>;
     close(): Promise<void>;
 }
--- lib/client/ftp/IFtpClientManager.d.ts
+++ lib/client/ftp/IFtpClientManager.d.ts
@@ -4,5 +4,5 @@ export interface IFtpClientManager {
     connect(): Promise<any>;
     close(): Promise<void>;
     put(origin: Readable | string, dest: string, createDir?: boolean): Promise<any>;
-    createSftpDirs(path: string): Promise<void>;
+    createSftpDirs(path: string): Promise<string>;
 }
