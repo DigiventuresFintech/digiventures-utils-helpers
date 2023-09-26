0605f86a5584b2e629fedcf2a51f8a288c026a90
Juan Beccaceci <juanbeccaceci@gmail.com>
2023-09-26 16:44:35 -0300

    Added timezone to getCurrentTime method

--- src/utils/DateUtils.ts
+++ src/utils/DateUtils.ts
@@ -1,10 +1,34 @@
 import dayjs from 'dayjs';
 
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
 export class DateUtils {
 
-  static getDateRange(difference: number): any {
+  static getDateRange(difference: number, timezone?: string): any {
+    if (timezone) {
+      dayjs.tz.setDefault(timezone);
+    }
+
     let now = dayjs();
 
+    const startDate = now.subtract(difference, 'day').startOf('day');
+    const endDate = now.subtract(difference, 'day').endOf('day');
+
+    return {
+      startDate: startDate.toISOString(),
+      endDate: endDate.toISOString(),
+    };
+  }
+
+  static getDateRangeAsGMT(difference: number, timezone?: string): any {
+    const _timezone: string = timezone ? timezone : 'America/Argentina/Buenos_Aires'
+
+
+    let now = dayjs();
     const startDate = now.subtract(difference, 'day').startOf('day');
     const endDate = now.subtract(difference, 'day').endOf('day');
 
