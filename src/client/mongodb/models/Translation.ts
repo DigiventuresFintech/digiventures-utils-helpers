36c56f737b179042824c0cdd7623b8db984c7d49
Juan Beccaceci <juanbeccaceci@gmail.com>
2023-12-06 11:41:32 -0300

    translations mongodb manager added

---
+++ src/client/mongodb/models/Translation.ts
@@ -0,0 +1,17 @@
+import mongoose, { Schema } from 'mongoose';
+
+export interface ITranslations extends mongoose.Document {
+  product: string,
+  referenceId: string,
+  language: string,
+  translations: any
+}
+
+const TranslationsSchema = new Schema({
+  product: { type: String, required: true },
+  referenceId: { type: String, required: true },
+  language: { type: String, required: true },
+  translations: Schema.Types.Mixed
+});
+
+export default mongoose.model<ITranslations>('multilanguage_capability', TranslationsSchema);
