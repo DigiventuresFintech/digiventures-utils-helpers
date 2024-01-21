7f57bd3b02e6846772af21bb4c0a4ee2cdc83e4f
Juan Beccaceci <juanbeccaceci@gmail.com>
2024-01-21 16:24:04 -0300

    Index table repository

---
+++ src/client/mongodb/impl/documents_index/IDocumentIndexManager.ts
@@ -0,0 +1,5 @@
+import { IBaseRepository } from '../../IBaseRepository';
+import { IDocumentIndex } from '../../models/DocumentIndex';
+
+export interface IDocumentIndexManager
+    extends IBaseRepository<IDocumentIndex> {}
---
+++ src/client/mongodb/models/DocumentIndex.ts
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+export interface IDocumentIndex extends mongoose.Document {
+    metadata: string;
+    tenantId: mongoose.Types.ObjectId;
+    workspaceId: mongoose.Types.ObjectId;
+    documentId: mongoose.Types.ObjectId;
+    createdAt: Date;
+    updatedAt: Date;
+}
