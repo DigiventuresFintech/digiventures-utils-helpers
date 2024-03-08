f77ad86703b8536deaf883df73325b008c4358bb
Juan Beccaceci <juanbeccaceci@gmail.com>
2024-03-08 10:50:51 -0300

    normalice elasticsearch version according to server version

--- src/client/elasticsearch/repository/BaseElasticRepositoryImpl.ts
+++ src/client/elasticsearch/repository/BaseElasticRepositoryImpl.ts
@@ -2,99 +2,96 @@ import { IElasticBaseRepository, TDoc } from './IElasticBaseRepository';
 import { Client } from '@elastic/elasticsearch';
 
 export class BaseElasticRepositoryImpl<T = any>
-    implements IElasticBaseRepository<T>
+  implements IElasticBaseRepository<T>
 {
-    private client: Client;
-    private readonly indexName: string;
+  private client: Client;
+  private readonly indexName: string;
 
-    constructor(_client: Client, _indexName: string) {
-        this.client = _client;
-        this.indexName = _indexName;
-    }
+  constructor(_client: Client, _indexName: string) {
+    this.client = _client;
+    this.indexName = _indexName;
+  }
 
-    async insertDocument<T = Record<string, any>>(
-        doc: T,
-        id?: string,
-    ): Promise<any> {
-        try {
-            return await this.client.index({
-                index: this.indexName,
-                body: doc,
-                id,
-            });
-        } catch (e) {
-            console.error('elasticsearch insert error', e);
-            throw e;
-        }
+  async insertDocument(doc: TDoc, id?: string): Promise<any> {
+    try {
+      return await this.client.index({
+        index: this.indexName,
+        body: doc,
+        id,
+      });
+    } catch (e) {
+      console.error('elasticsearch insert error', e);
+      throw e;
     }
+  }
 
-    async updateById(id: string, body: TDoc): Promise<any> {
-        try {
-            return await this.client.update({
-                index: this.indexName,
-                id: id,
-                body,
-            });
-        } catch (e) {
-            console.error('elasticsearch update error', e);
-            throw e;
-        }
+  async updateById(id: string, body: TDoc): Promise<any> {
+    try {
+      return await this.client.update({
+        index: this.indexName,
+        id: id,
+        body,
+      });
+    } catch (e) {
+      console.error('elasticsearch update error', e);
+      throw e;
     }
+  }
 
-    async updateByIndex(id: string, body: TDoc): Promise<any> {
-        try {
-            return await this.client.index({
-                index: this.indexName,
-                id: id,
-                body,
-            });
-        } catch (e) {
-            console.error('elasticsearch index error', e);
-            throw e;
-        }
+  async updateByIndex(id: string, body: TDoc): Promise<any> {
+    try {
+      return await this.client.index({
+        index: this.indexName,
+        id: id,
+        body,
+      });
+    } catch (e) {
+      console.error('elasticsearch index error', e);
+      throw e;
     }
+  }
 
-    async createAlias(aliasName: string, indexName: string[]): Promise<any> {
-        try {
-            const response = await this.client.indices.putAlias({
-                index: indexName,
-                name: aliasName,
-            });
-            console.log('alias created successfully', response);
-            return response;
-        } catch (e) {
-            console.error('alias created error', e);
-            throw e;
-        }
+  async createAlias(aliasName: string, indexName: string[]): Promise<any> {
+    try {
+      const response = await this.client.indices.putAlias({
+        index: indexName,
+        name: aliasName,
+      });
+      console.log('alias created successfully', response);
+      return response;
+    } catch (e) {
+      console.error('alias created error', e);
+      throw e;
     }
+  }
 
-    async addIndexToAlias(indexName: string, aliasName: string): Promise<any> {
-        try {
-            const response = await this.client.indices.updateAliases({
-                body: {
-                    actions: [{ add: { index: indexName, alias: aliasName } }],
-                },
-            });
-            console.log(
-                `Index '${indexName}' attached to alias '${aliasName}'`,
-                response,
-            );
-            return response;
-        } catch (e) {
-            console.error('error updating index', e);
-            throw e;
-        }
+  async addIndexToAlias(indexName: string, aliasName: string): Promise<any> {
+    try {
+      const response = await this.client.indices.updateAliases({
+        body: {
+          actions: [{ add: { index: indexName, alias: aliasName } }],
+        },
+      });
+      console.log(
+        `Index '${indexName}' attached to alias '${aliasName}'`,
+        response,
+      );
+      return response;
+    } catch (e) {
+      console.error('error updating index', e);
+      throw e;
     }
+  }
 
-    async deleteById(id: string): Promise<any> {
-        try {
-            return await this.client.delete({
-                index: this.indexName,
-                id: id,
-            });
-        } catch (e) {
-            console.error('elasticsearch delete error', e);
-            throw e;
-        }
+  async deleteById(id: string): Promise<any> {
+    try {
+      return await this.client.delete({
+        index: this.indexName,
+        id: id,
+      });
+    } catch (e) {
+      console.error('elasticsearch delete error', e);
+      throw e;
     }
+  }
 }
--- src/client/elasticsearch/repository/IElasticBaseRepository.ts
+++ src/client/elasticsearch/repository/IElasticBaseRepository.ts
@@ -1,12 +1,12 @@
 export interface TDoc<T = Record<string, any>> {
-    doc: T;
+  doc: T;
 }
 
 export interface IElasticBaseRepository<T> {
-    insertDocument<T = Record<string, any>>(doc: T): Promise<any>;
-    updateById(id: string, body: TDoc): Promise<any>;
-    updateByIndex(id: string, body: TDoc): Promise<any>;
-    createAlias(aliasName: string, indexName: string[]): Promise<any>;
-    addIndexToAlias(indexName: string, aliasName: string): Promise<any>;
-    deleteById(id: string): Promise<any>;
+  insertDocument(doc: TDoc): Promise<any>;
+  updateById(id: string, body: TDoc): Promise<any>;
+  updateByIndex(id: string, body: TDoc): Promise<any>;
+  createAlias(aliasName: string, indexName: string[]): Promise<any>;
+  addIndexToAlias(indexName: string, aliasName: string): Promise<any>;
+  deleteById(id: string): Promise<any>;
 }
